55406e9d1c83437d77a28637c5f93f8fa19a864f
madinakhatukhova <Madina-Khat@yandex.ru>
2025-03-05 20:28:06 +0300

    2

---
+++ src/main/java/pro/sky/question/course/work/service/JavaQuestionService.java
@@ -0,0 +1,60 @@
+package pro.sky.question.course.work.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.question.course.work.domain.Question;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+@Service
+public class JavaQuestionService implements QuestionService {
+
+    private final Set<Question> questions;
+
+    public JavaQuestionService() {
+
+        this.questions = new HashSet<>();
+    }
+
+    @Override
+    public Question add(String question, String answer) {
+        Question quest = new Question(question, answer);
+        questions.add(quest);
+        return quest;
+    }
+
+    @Override
+    public Question add(Question question) {
+        questions.add(question);
+        return question;
+    }
+
+    @Override
+    public Question remove(Question question) {
+        if (!questions.contains(question)) {
+            throw new NotFoundException();
+        }
+        questions.remove(question);
+        return question;
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return Collection.unmodifiableSet(questions);
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+        int random = new Random().nextInt(questions.size());
+        int i = 0;
+        for (Question question : questions) {
+            if (i == random) {
+                return question;
+            }
+            i++;
+        }
+        return null;
+    }
+}
